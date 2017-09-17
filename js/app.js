99574057934142d4869d38649372ba00ccab4e8c
kwanwooi25 <kwanwoo.jeong@gmail.com>
2017-09-17 20:34:11 +0900

    commit 4 - js
    - added js for modal display

--- js/app.js
+++ js/app.js
@@ -3,27 +3,31 @@
 ===========================================================*/
 
 // Variables
-var employeesArray = [];
+let employeesArray = [];
 const employeeList = document.getElementById('employeeList');
+const modal = document.getElementById('modal');
+const modalClose = document.getElementById('modal__close');
 
 // Get data from Random User Generator API
 $.ajax({
   url: 'https://randomuser.me/api/?results=12&nat=us,gb,au',
   dataType: 'json',
-  success: addEmployees
+  success: (data) => {
+    employeesArray = data.results;
+    updateEmployeeList(employeesArray);
+  }
 });
 
-function addEmployees(data) {
-  employeesArray = data.results;
-  var html = '';
+function updateEmployeeList(data) {
+  let html = '';
 
-  for (var i = 0; i < employeesArray.length; i++) {
-    var thumbnail = employeesArray[i].picture.medium;
-    var name = employeesArray[i].name.first.capitalize() +
-              ' ' +
-              employeesArray[i].name.last.capitalize();
-    var email = employeesArray[i].email;
-    var city = employeesArray[i].location.city.capitalize();
+  for (let i = 0; i < employeesArray.length; i++) {
+    let thumbnail = employeesArray[i].picture.medium;
+    let name = capitalize(employeesArray[i].name.first +
+                          ' ' +
+                          employeesArray[i].name.last);
+    let email = employeesArray[i].email;
+    let city = capitalize(employeesArray[i].location.city);
 
     html += `
       <li class="employeeList__item">
@@ -46,3 +50,69 @@ function addEmployees(data) {
 
   employeeList.innerHTML = html;
 }
+
+// hide modal on modal close button click
+function hideModal() {
+  modal.style.display = 'none';
+}
+
+employeeList.addEventListener('click', (e) => {
+  let index = getIndex(e.target.parentNode.parentNode);
+  let html = '';
+
+  let image = employeesArray[index].picture.large;
+  let name = capitalize(employeesArray[index].name.first +
+                        ' ' +
+                        employeesArray[index].name.last);
+  let email = employeesArray[index].email;
+  let city = capitalize(employeesArray[index].location.city);
+  let phone = employeesArray[index].phone;
+  let address = capitalize(employeesArray[index].location.street) +
+                ', ' +
+                capitalize(employeesArray[index].location.state) +
+                ' ' +
+                employeesArray[index].location.postcode;
+  let birthday = employeesArray[index].dob.substring(8, 10) +
+                '/' +
+                employeesArray[index].dob.substring(5, 7) +
+                '/' +
+                employeesArray[index].dob.substring(0, 4);
+  let statePrev = "";
+  let stateNext = "";
+
+  if(index === 0) {
+    statePrev = "disabled";
+  }
+
+  if(index === employeesArray.length - 1) {
+    stateNext = "disabled";
+  }
+  
+  html = `
+    <div class="modal__container">
+      <div class="modal__closeContainer">
+        <span id="modal__close" class="modal__close" onclick="hideModal();">&times;</span>
+      </div>
+      <div class="modal__imageContainer">
+        <img class="modal__image" src="${image}" alt="${name}">
+      </div>
+      <div class="modal__textContainer">
+        <p class="modal__text modal__name">${name}</p>
+        <a class="modal__link modal__email" href="mailto:${email}">${email}</a>
+        <p class="modal__text modal__city">${city}</p>
+      </div>
+      <div class="modal__textContainer">
+        <a class="modal__link modal__phone" href="tel:${phone}">${phone}</a>
+        <p class="modal__text modal__address">${address}</p>
+        <p class="modal__text modal__birthday">Birthday: ${birthday}</p>
+      </div>
+      <div class="modal__buttonContainer">
+        <button class="btn__modal prev" ${statePrev}>PREV</button>
+        <button class="btn__modal next" ${stateNext}>NEXT</button>
+      </div>
+    </div>
+  `;
+
+  modal.innerHTML = html;
+  modal.style.display = 'block';
+})
--- js/prototype.js
+++ js/prototype.js
@@ -1,4 +1,18 @@
-// to capitalize first letter of names
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-};
+// to capitalize first letter
+function capitalize(str) {
+  return str.toLowerCase()
+            .replace(/^\w|\s\w/g,
+                    function (letter) {
+                      return letter.toUpperCase();
+                    })
+}
+
+// to get the index of a node
+function getIndex(node) {
+  let children = node.parentNode.children;
+  let index;
+  for (let i = 0; i < children.length; i++) {
+    if (node === children[i]) index = i;
+  }
+  return index;
+}
