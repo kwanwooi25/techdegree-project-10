16b52c4c38f6d794a9f11cc5696b8f4301035509
kwanwooi25 <kwanwoo.jeong@gmail.com>
2017-09-18 08:55:10 +0900

    commit 6 - js (event for 'prev', 'next')

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Variables
 let employeesArray = [];
 let employeesArrayFiltered = [];
+let currentIndex;
 const employeeList = document.getElementById('employeeList');
 const modal = document.getElementById('modal');
 const modalClose = document.getElementById('modal__close');
@@ -11,43 +12,64 @@ $.ajax({
   url: 'https://randomuser.me/api/?results=12&nat=us',
   dataType: 'json',
   success: (data) => {
-    employeesArray = data.results;
+    employeesArrayAll = data.results;
     employeesArrayFiltered = data.results;
-    updateEmployeeList(employeesArray);
+    updateEmployeeList('');
+    console.log(employeesArrayAll);
   }
 });
 
-function updateEmployeeList(data) {
+function updateEmployeeList(searchValue) {
   let html = '';
 
-  for (let i = 0; i < employeesArrayFiltered.length; i++) {
-    let thumbnail = employeesArrayFiltered[i].picture.medium;
-    let name = capitalize(employeesArrayFiltered[i].name.first +
-                          ' ' +
-                          employeesArrayFiltered[i].name.last);
-    let email = employeesArrayFiltered[i].email;
-    let city = capitalize(employeesArrayFiltered[i].location.city);
+  employeesArrayFiltered = [];
+  employeeList.innerHTML = '';
+  for (let i = 0; i < employeesArrayAll.length; i++) {
+    let name = employeesArrayAll[i].name.first +
+              ' ' +
+              employeesArrayAll[i].name.last;
+    if (name.includes(searchValue)) {
+      html += appendLItoEmployeeList(employeesArrayAll[i]);
+    }
+  }
 
-    html += `
-      <li class="employeeList__item">
-        <div class="employeeList__wrapperRow">
-          <div class="employeeList__wrapperImage">
-            <img class="image__employeeList" src="${thumbnail}" alt="${name}">
-          </div>
-          <div class="employeeList__wrapperText">
-            <p class="employeeList__name">${name}</p>
-            <p class="employeeList__email">${email}</p>
-            <p class="employeeList__city">${city}</p>
-          </div>
-        </div>
-        <div class="employeeList__wrapperRow">
-          <button class="btn btn__employeeList">More Info</button>
-        </div>
-      </li>
+  if (employeesArrayFiltered.length === 0) {
+    html = `
+      <p class="employeeList__noResult">
+        Cannot find any employee!
+      </p>
     `;
   }
 
   employeeList.innerHTML = html;
+}
+
+function appendLItoEmployeeList(employee) {
+  employeesArrayFiltered.push(employee);
+  let thumbnail = employee.picture.medium;
+  let name = capitalize(employee.name.first +
+                        ' ' +
+                        employee.name.last);
+  let email = employee.email;
+  let city = capitalize(employee.location.city);
+
+  return `
+    <li class="employeeList__item">
+      <div class="employeeList__wrapperRow">
+        <div class="employeeList__wrapperImage">
+          <img class="image__employeeList" src="${thumbnail}" alt="${name}">
+        </div>
+        <div class="employeeList__wrapperText">
+          <p class="employeeList__name">${name}</p>
+          <p class="employeeList__email">${email}</p>
+          <p class="employeeList__city">${city}</p>
+        </div>
+      </div>
+      <div class="employeeList__wrapperRow">
+        <button class="btn btn__employeeList">More Info</button>
+      </div>
+    </li>
+  `;
 }
 
 // hide modal on modal close button click
@@ -55,81 +77,95 @@ function hideModal() {
   modal.style.display = 'none';
 }
 
+// show modal
+/*=============================================================
+ Show modal
+ 1. when 'more info' button clicked
+ 2. when 'prev' or 'next' button clicked
+=============================================================*/
+function showModal(index) {
+  let employee = employeesArrayFiltered[index];
+  let html = '';
+
+  let image = employee.picture.large;
+  let name = capitalize(employee.name.first +
+                        ' ' +
+                        employee.name.last);
+  let email = employee.email;
+  let city = capitalize(employee.location.city);
+  let phone = employee.phone;
+  let address = capitalize(employee.location.street) +
+                ', ' +
+                capitalize(employee.location.state) +
+                ' ' +
+                employee.location.postcode;
+  let birthday = employee.dob.substring(8, 10) +
+                '/' +
+                employee.dob.substring(5, 7) +
+                '/' +
+                employee.dob.substring(0, 4);
+  let statePrev = "";
+  let stateNext = "";
+
+  // disable 'prev' button for 1st employee
+  if(index === 0) statePrev = "disabled";
+  // disable 'next' button for last employee
+  if(index === employeesArrayFiltered.length - 1) {
+    stateNext = "disabled";
+  }
+
+  html = `
+    <div class="modal__container">
+      <div class="modal__closeContainer">
+        <span id="modal__close" class="modal__close">&times;</span>
+      </div>
+      <div class="modal__imageContainer">
+        <img class="modal__image" src="${image}" alt="${name}">
+      </div>
+      <div class="modal__textContainer">
+        <p class="modal__text modal__name">${name}</p>
+        <a class="modal__link modal__email" href="mailto:${email}">${email}</a>
+        <p class="modal__text modal__city">${city}</p>
+      </div>
+      <div class="modal__textContainer">
+        <a class="modal__link modal__phone" href="tel:${phone}">${phone}</a>
+        <p class="modal__text modal__address">${address}</p>
+        <p class="modal__text modal__birthday">Birthday: ${birthday}</p>
+      </div>
+      <div class="modal__buttonContainer">
+        <button class="btn__modal prev" ${statePrev}>PREV</button>
+        <button class="btn__modal next" ${stateNext}>NEXT</button>
+      </div>
+    </div>
+  `;
+
+  modal.innerHTML = html;
+  modal.style.display = 'block';
+}
+
+// on 'more infor' button clicked
 employeeList.addEventListener('click', (e) => {
   if (e.target.tagName === 'BUTTON') {
-    let index = getIndex(e.target.parentNode.parentNode);
-    let employee = employeesArrayFiltered[index];
-    let html = '';
-
-    let image = employee.picture.large;
-    let name = capitalize(employee.name.first +
-                          ' ' +
-                          employee.name.last);
-    let email = employee.email;
-    let city = capitalize(employee.location.city);
-    let phone = employee.phone;
-    let address = capitalize(employee.location.street) +
-                  ', ' +
-                  capitalize(employee.location.state) +
-                  ' ' +
-                  employee.location.postcode;
-    let birthday = employee.dob.substring(8, 10) +
-                  '/' +
-                  employee.dob.substring(5, 7) +
-                  '/' +
-                  employee.dob.substring(0, 4);
-    let statePrev = "";
-    let stateNext = "";
-
-    if(index === 0) {
-      statePrev = "disabled";
-    }
-
-    if(index === employeesArrayFiltered.length - 1) {
-      stateNext = "disabled";
-    }
-
-    html = `
-      <div class="modal__container">
-        <div class="modal__closeContainer">
-          <span id="modal__close" class="modal__close" onclick="hideModal();">&times;</span>
-        </div>
-        <div class="modal__imageContainer">
-          <img class="modal__image" src="${image}" alt="${name}">
-        </div>
-        <div class="modal__textContainer">
-          <p class="modal__text modal__name">${name}</p>
-          <a class="modal__link modal__email" href="mailto:${email}">${email}</a>
-          <p class="modal__text modal__city">${city}</p>
-        </div>
-        <div class="modal__textContainer">
-          <a class="modal__link modal__phone" href="tel:${phone}">${phone}</a>
-          <p class="modal__text modal__address">${address}</p>
-          <p class="modal__text modal__birthday">Birthday: ${birthday}</p>
-        </div>
-        <div class="modal__buttonContainer">
-          <button class="btn__modal prev" ${statePrev}>PREV</button>
-          <button class="btn__modal next" ${stateNext}>NEXT</button>
-        </div>
-      </div>
-    `;
-
-    modal.innerHTML = html;
-    modal.style.display = 'block';
+    currentIndex = getIndex(e.target.parentNode.parentNode);
+    showModal(currentIndex);
   }
+})
+
+modal.addEventListener('click', (e) => {
+  let clicked = e.target;
+
+  // 'prev', 'next' button click event
+  if (clicked.tagName === 'BUTTON') {
+    if (clicked.classList.contains('prev')) currentIndex -= 1;
+    if (clicked.classList.contains('next')) currentIndex += 1;
+    showModal(currentIndex);
+  }
+
+  // hide modal on 'modal__close' button click
+  if (clicked.id === 'modal__close') hideModal();
 })
 
 inputSearchEmployee.addEventListener('keyup', (e) => {
   let searchValue = e.target.value.toLowerCase();
-  employeesArrayFiltered = [];
-  for (let i = 0; i < employeesArray.length; i++) {
-    let name = employeesArray[i].name.first +
-              ' ' +
-              employeesArray[i].name.last;
-    if (name.includes(searchValue)) {
-      employeesArrayFiltered.push(employeesArray[i]);
-      console.log(employeesArray[i]);
-    }
-  }
-  updateEmployeeList(employeesArrayFiltered);
+  updateEmployeeList(searchValue);
 })
